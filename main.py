1e96997dfa43c3ce078c45ef270ee70cee5f5c1e
nasstyzay <nasstyzay@mail.ru>
2023-12-15 15:24:42 +0300

    add

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ with open('2.txt', 'w', encoding='utf-8') as f:
 with open('Final file', 'a', encoding='utf-8') as f:
     f.write('2.txt\n1\nСтрока номер 1 файла номер 2\n1.txt\n2\nСтрока номер 1 файла номер 1\nСтрока номер 2 файла номер 1')
 
+
+
